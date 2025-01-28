b8442a05bcc92ac6f7e5fccfef4f9a35805103a6
Jon McLean <jon@mclean.one>
2025-01-28 19:33:33 +1100

    Removed unnecessary mock

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,13 +4,5 @@ from fastapi.testclient import TestClient
 from src.api.api import app
 
 @pytest.fixture
-def mock_life_span():
-    app.mongodb_client = None
-    app.database = None
-    yield
-    app.mongodb_client = None
-    app.database = None
-
-@pytest.fixture
 def client():
     return TestClient(app)
